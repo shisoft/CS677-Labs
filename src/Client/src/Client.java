dce998c0c47054db70a0bab698d01c894e7e3c8a
chenhao huang <chenhaohuang@chenhaos-MacBook-Pro.local>
2020-04-03 18:44:00 -0400

    java 11 to jdk 8

--- src/Client/src/Client.java
+++ src/Client/src/Client.java
@@ -1,9 +1,10 @@
+
 import javax.swing.plaf.basic.BasicPasswordFieldUI;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.URI;
-import java.net.http.HttpResponse;
+import java.io.InputStreamReader;
+import java.net.*;
+
 import java.util.Scanner;
 
 public class Client {
@@ -12,7 +13,17 @@ public class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner in = new Scanner(System.in);
         String lastline = "null";
-        HttpClient client = HttpClient.newHttpClient();
+      // URL yahoo = new URL("http://www.yahoo.com/");
+//        URLConnection yc = yahoo.openConnection();
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(
+//                        yc.getInputStream()));
+//        String inputLine;
+//
+//        while ((inputLine = in.readLine()) != null)
+//            System.out.println(inputLine);
+//        in.close();
+
         while(!lastline.toLowerCase().equals("quit")){
             System.out.println("Welcome, what would you like to do? search, lookup, buy？type in or indicate by number 1,2,3 or quit");
             switch (in.nextLine().toLowerCase()){
@@ -22,41 +33,52 @@ public class Client {
                     lastline = in.nextLine().toLowerCase();
                     lastline = lastline.replace(" ","-");
 
-                    HttpRequest request = HttpRequest.newBuilder()
-                            .uri(URI.create("http://0.0.0.0:34843/search/"+lastline))
-                            .build();
+                    URL request1 = new URL("http://0.0.0.0:34843/search/"+lastline);
+                    URLConnection yc = request1.openConnection();
+                    BufferedReader r1 = new BufferedReader(new InputStreamReader(
+                        yc.getInputStream()));
 
-                    HttpResponse<String> response = client.send(request,
-                            HttpResponse.BodyHandlers.ofString());
+                    String inputLine;
 
-                    System.out.println(response.body());
+                    while ((inputLine = r1.readLine()) != null)
+                        System.out.println(inputLine);
+                    r1.close();
                     break;
 
                 case "2":
                 case "lookup":
                     System.out.println("What is the item number?");
                     lastline = in.nextLine().toLowerCase();
-                    HttpRequest requestlook = HttpRequest.newBuilder()
-                            .uri(URI.create("http://0.0.0.0:34843/lookup/"+lastline))
-                            .build();
 
-                    HttpResponse<String> responselook = client.send(requestlook,
-                            HttpResponse.BodyHandlers.ofString());
+                    URL request2 = new URL("http://0.0.0.0:34843/lookup/"+lastline);
+                    URLConnection yc2 = request2.openConnection();
+                    BufferedReader r2 = new BufferedReader(new InputStreamReader(
+                            yc2.getInputStream()));
 
-                    System.out.println(responselook.body());
+                    String inputLine2;
+
+                    while ((inputLine2 = r2.readLine()) != null)
+                        System.out.println(inputLine2);
+                    r2.close();
+
                     break;
                 case "3":
                 case "buy":
                     System.out.println("What is the item number?");
                     lastline = in.nextLine().toLowerCase();
-                    HttpRequest requestbuy = HttpRequest.newBuilder()
-                            .uri(URI.create("http://0.0.0.0:34843/buy/"+lastline))
-                            .build();
 
-                    HttpResponse<String> responsebuy = client.send(requestbuy,
-                            HttpResponse.BodyHandlers.ofString());
 
-                    System.out.println(responsebuy.body());
+                    URL request3 = new URL("http://0.0.0.0:34843/buy/"+lastline);
+                    URLConnection yc3 = request3.openConnection();
+                    BufferedReader r3 = new BufferedReader(new InputStreamReader(
+                            yc3.getInputStream()));
+
+                    String inputLine3;
+
+                    while ((inputLine3 = r3.readLine()) != null)
+                        System.out.println(inputLine3);
+                    r3.close();
+
                     break;
                 case "quit":
                     lastline = "quit";
