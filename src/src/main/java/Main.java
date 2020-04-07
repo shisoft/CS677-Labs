f64ed45365dad5e340f49df16161e464ff216dac
chenhao huang <chenhaohuang@chenhaos-MacBook-Pro.local>
2020-04-07 00:07:20 -0400

    port

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        port(34843);
+        port(34842);
 //todo log
         //todo At the beginning of each run, the Catalog log should
         // include one or more entries recording the initial state of
