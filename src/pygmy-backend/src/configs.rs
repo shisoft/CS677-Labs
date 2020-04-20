90d20ed6c985bcc053c2ab2fa73564546cfd9570
Hao Shi <shisoftgenius@gmail.com>
2020-04-20 05:00:13 -0400

    RSM for order server

--- src/pygmy-backend/src/configs.rs
+++ src/pygmy-backend/src/configs.rs
@@ -9,4 +9,10 @@ lazy_static! {
         env::var("ORDER_SERVER_PORT").unwrap_or("34802".to_string());
     pub static ref CAT_SERVER_ADDR: String =
         env::var("CAT_SERVER_ADDR").unwrap_or("127.0.0.1".to_string());
+    pub static ref SERVER_ADDR: String =
+        env::var("SERVER_ADDR").unwrap_or("127.0.0.1".to_string());
+    pub static ref CAT_SERVER_LIST: Vec<String> =
+        env::var("CAT_SERVER_LIST").unwrap_or("".to_string()).split(",").map(|s| s.trim()).collect();
+    pub static ref ORDER_SERVER_LIST: Vec<String> =
+        env::var("ORDER_SERVER_LIST").unwrap_or("".to_string()).split(",").map(|s| s.trim()).collect();
 }
--- src/pygmy-backend/src/order.rs
+++ src/pygmy-backend/src/order.rs
@@ -2,6 +2,8 @@
 extern crate diesel;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate bifrost;
 extern crate dotenv;
 
 use crate::configs::*;
@@ -15,11 +17,41 @@ use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use log::*;
 use serde::{Deserialize, Serialize};
+use bifrost::raft::state_machine::StateMachineCtl;
+use reqwest::header::SERVER;
+use bifrost::raft::{RaftService, Options, Storage, DEFAULT_SERVICE_ID};
+use bifrost::rpc::Server;
 
 mod configs;
 mod data;
 mod models;
 mod schema;
+
+struct ReplicatedOrderLog;
+
+// Define the interface of replicated state machine here
+// The state machine only respnsible for logging the order to the database, because replicas cannot
+// check with catalog for stock on each of the nodes
+raft_state_machine! {
+    def cmd log_order(item: i32, amount: i32, total: f32);
+}
+
+// Define the implementation of the RSM, which is to write the log to database
+impl StateMachineCmds for ReplicatedOrderLog {
+    fn log_order(&mut self, item: i32, amount: i32, total: f32) -> BoxFuture<()> {
+        info!("RSM replicating the order log, item {}, amount {}, total {}", item_id, amount, total);
+        use schema::order::dsl::*;
+        let conn = establish_connection();
+        diesel::insert_into(order)
+            .values(&NewOrder {
+                item,
+                amount,
+                total,
+            })
+            .execute(&conn);
+        future::ready(()).boxed()
+    }
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -27,6 +59,14 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     // Initialize logger
     simple_logger::init().unwrap();
+
+    // Initialize raft server and their service
+    // The TCP server responsible for Raft use a dedicated binary protocol, require its own server
+    // apart from the HTTP Restful server
+    tokio::spawn(async {
+        start_raft_state_machine().await
+    });
+
     HttpServer::new(|| {
         App::new()
             // Setup logging middleware for HTTP server
@@ -39,6 +79,27 @@ async fn main() -> std::io::Result<()> {
     // Run the server
     .run()
     .await
+}
+
+async fn start_raft_state_machine() {
+    let raft_addr = format!("{}:{}", *SERVER_ADDR, *RAFT_SERVER_PORT)
+    let raft_service = RaftService::new(Options {
+        storage: Storage::default(),
+        address: raft_addr.clone(),
+        service_id: DEFAULT_SERVICE_ID,
+    });
+    // Initialize the RPC server for Raft
+    let server = Server::new(&raft_addr);
+    // Register the Raft service to the RPC server
+    server.register_service(DEFAULT_SERVICE_ID, &raft_service).await;
+    // Start the RPC server
+    Server::listen_and_resume(&server).await;
+    // Start the raft service
+    RaftService::start(&raft_service).await;
+    // Register the state machine to the raft service
+    raft_service.register_state_machine(Box::new(ReplicatedOrderLog));
+    // Probe and join the cluster. If no live node, it will bootstrap
+    raft_service.probe_and_join(&*ORDER_SERVER_LIST).await;
 }
 
 async fn order_handler(req: HttpRequest) -> impl Responder {
@@ -81,15 +142,7 @@ async fn order_handler(req: HttpRequest) -> impl Responder {
                     "Order transaction for {} successful, log transaction",
                     item_id
                 );
-                use schema::order::dsl::*;
-                let conn = establish_connection();
-                diesel::insert_into(order)
-                    .values(&NewOrder {
-                        item: item_id,
-                        amount: order_amount,
-                        total: lookup_item.price * order_amount as f32,
-                    })
-                    .execute(&conn);
+                // TODO: invoke the RSM
                 return HttpResponse::Ok().json(true);
             } else {
                 info!("Order transaction for {} failed, aborted", item_id);
@@ -101,4 +154,23 @@ async fn order_handler(req: HttpRequest) -> impl Responder {
         info!("Cannot find item {} in catalog server", item_id);
     }
     HttpResponse::Ok().json(false)
+}
+
+impl StateMachineCtl for ReplicatedOrderLog {
+    // Auto generate stub dispatcher
+    raft_sm_complete!();
+    // Unique id for this state machine
+    fn id(&self) -> u64 {
+        1
+    }
+
+    fn snapshot(&self) -> Option<Vec<u8>> {
+        // We are not going to use this feature in the project
+        unimplemented!();
+    }
+
+    fn recover(&mut self, data: Vec<u8>) -> BoxFuture<'_, ()> {
+        // We are not going to use this feature in the project
+        unimplemented!()
+    }
 }
